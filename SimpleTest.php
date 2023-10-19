805dd7db62b75aeb647c6e7b4c89128aa9a1ee57
rayrayaray <rayasya.dziqi@gmail.com>
2023-10-19 14:29:02 +0700

    update penjelasan

--- SimpleTest.php
+++ SimpleTest.php
@@ -1,17 +1,25 @@
 <?php
+//menggunakan library TestCase yang tersedia pada PHPunit
 use PHPUnit\Framework\TestCase;
 
-
+//menggunakan class yang akan kita test
 require_once "WordCount.php";
 
+//membuat class yang inherit dari class TestCase PHPunit
 class SimpleTest extends TestCase
 {
     public function testCountWords()
     {
+        //instansiasi class WordCount menjadi objek berupa $wc
         $wc = new WordCount();
-        $testSentence = "Nama Saya Rayasya";
+
+        //membuat variable $testSentence dengan isian string yang nantinya akan diuji
+        $testSentence = "Nama Saya Rayasya"; // 3 kata
+
+        /*membuat variable $wordCount yang isinya adalah return hasil hitungan (count) dari function countWords pada class WordCount dengan memasukkan value pada parameter berupa string yang telah dibuat diatas*/
         $wordCount = $wc->countWords($testSentence);
 
+        /*memanggil function assertEquals yang berfungsi untuk mengecek apakah value pada parameter pertama sama dengan value pada parameter kedua, yang dimana value dari parameter kedua adalah hasil count dari function countWords pada class WordCount*/
         $this->assertEquals(3, $wordCount);
     }
 }
--- WordCount.php
+++ WordCount.php
@@ -1,9 +1,13 @@
 <?php
 class WordCount
 {
+    //membuat function countWords dengan parameter
     public function countWords($sentence)
     {
         return count(explode(" ", $sentence));
+        /* mengembalikan hasil function countWords berupa hasil hitung explode, fungsi explode adalah
+        function yang dimana akan memisah string.pada kasus kali ini yang di explode adalah pada
+        $sentence setiap bertemu karakter spasi */
     }
 }
 ?>
